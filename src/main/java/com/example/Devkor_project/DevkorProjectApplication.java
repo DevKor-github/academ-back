8113e4de606f803204f9d237f1cb9096513b955f
YunJaeHoon <sleepzzz214@naver.com>
2024-03-28 22:25:11 +0900

    init

---
+++ src/main/java/com/example/Devkor_project/DevkorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Devkor_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevkorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevkorProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Devkor_project/DevkorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Devkor_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevkorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
