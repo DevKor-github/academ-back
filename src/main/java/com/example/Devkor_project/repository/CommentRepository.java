ed70b599461f2d672ef0df7bb6e8d75257a37b39
YunJaeHoon <sleepzzz214@naver.com>
2024-05-27 17:27:53 +0900

    FTR: 강의평 작성 시작 컨트롤러, 서비스 제작

--- src/main/java/com/example/Devkor_project/repository/CommentRepository.java
+++ src/main/java/com/example/Devkor_project/repository/CommentRepository.java
@@ -1,7 +1,17 @@
 package com.example.Devkor_project.repository;
 
+import com.example.Devkor_project.entity.Bookmark;
 import com.example.Devkor_project.entity.Comment;
+import com.example.Devkor_project.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long>
+{
+    @Query(value = "SELECT * FROM comment WHERE profile_id = :profile_id AND course_id = :course_id", nativeQuery = true)
+    List<Comment> searchComment(@Param("profile_id") Long profile_id, @Param("course_id") Long course_id);
 }
