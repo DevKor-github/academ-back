b6d7f2e19088366ea73757422c29768a0cf4885d
Minseok Choe <95574695+stonechoe@users.noreply.github.com>
2024-06-13 21:31:03 +0900

    Revert "FIX: 로그인, 로그아웃 리다이렉트 제거"

--- src/main/java/com/example/Devkor_project/security/CustomAuthFailureHandler.java
+++ src/main/java/com/example/Devkor_project/security/CustomAuthFailureHandler.java
@@ -1,8 +1,5 @@
 package com.example.Devkor_project.security;
 
-import com.example.Devkor_project.configuration.VersionProvider;
-import com.example.Devkor_project.dto.ResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,34 +17,29 @@ import com.example.Devkor_project.configuration.VersionProvider;
 import java.io.IOException;
 
 @Component
-public class CustomAuthFailureHandler implements AuthenticationFailureHandler
-{
+public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
     @Autowired
     VersionProvider versionProvider;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
-        String errorMessage = null;
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException exception) throws IOException, ServletException {
+        String errorCode = null;
 
         if (exception instanceof BadCredentialsException || exception instanceof InternalAuthenticationServiceException)
-            errorMessage = "WRONG";         // 이메일 또는 비밀번호가 틀린 경우
+            errorCode = "WRONG"; // 이메일 또는 비밀번호가 틀린 경우
         else if (exception instanceof DisabledException)
-            errorMessage = "DISABLED";      // 해당 계정이 비활성화된 경우
+            errorCode = "DISABLED"; // 해당 계정이 비활성화된 경우 (현재 사용 안함)
         else
-            errorMessage = "UNEXPECTED";    // 예기치 못한 에러가 발생한 경우
-
-        ResponseDto.Error dto = ResponseDto.Error.builder()
-                .data(null)
-                .message(errorMessage)
-                .version(versionProvider.getVersion())
-                .build();
+            errorCode = "UNEXPECTED"; // 예기치 못한 에러가 발생한 경우
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(dto));
+        response.getWriter().print("{\"status\":\"FAILURE\", \"reason\":\"" + errorCode + "\", \"version\":\""
+                + versionProvider.getVersion() + "\"}");
         response.getWriter().flush();
+
+        // super.onAuthenticationFailure(request, response, exception);
     }
 }
--- src/main/java/com/example/Devkor_project/security/CustomAuthSuccessHandler.java
+++ src/main/java/com/example/Devkor_project/security/CustomAuthSuccessHandler.java
@@ -1,34 +1,40 @@
 package com.example.Devkor_project.security;
 
-import com.example.Devkor_project.configuration.VersionProvider;
-import com.example.Devkor_project.dto.ResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.example.Devkor_project.configuration.VersionProvider;
+import com.example.Devkor_project.dto.ResponseDto;
 
 import java.io.IOException;
 
-public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler
-{
-    @Autowired VersionProvider versionProvider;
+@Component
+public class CustomAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    VersionProvider versionProvider;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException
-    {
-        ResponseDto.Success dto = ResponseDto.Success.builder()
-                .data(null)
-                .message("로그인을 성공하였습니다.")
-                .version(versionProvider.getVersion())
-                .build();
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
 
         response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(dto));
+        response.getWriter().print("{\"status\":\"SUCCESS\", \"version\":\"" + versionProvider.getVersion() + "\"}");
         response.getWriter().flush();
+
+        super.onAuthenticationSuccess(request, response, authentication);
     }
 }
