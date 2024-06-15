139a68c1df53e1972bc8198ae347b011e42d7ece
duru100470 <zenpia@naver.com>
2024-06-15 16:00:11 +0900

    v0.3.17-alpha
    CORS 테스트서버 주소 추가

--- src/main/java/com/example/Devkor_project/configuration/WebConfig.java
+++ src/main/java/com/example/Devkor_project/configuration/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedOrigins("https://localhost:4000", "http://localhost:4000","https://localhost:3000", "http://localhost:3000", "https://127.0.0.1:4000",
                         "http://127.0.0.1:4000","https://127.0.0.1:3000",
-                        "http://127.0.0.1:3000", "https://academ-frontend.vercel.app") // added more origin
+                        "http://127.0.0.1:3000", "https://academ-frontend.vercel.app", "https://13.124.249.107.nip.io") // added more origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE"); // Wildcard method not works in some browsers, and HTTPS
     }
 
