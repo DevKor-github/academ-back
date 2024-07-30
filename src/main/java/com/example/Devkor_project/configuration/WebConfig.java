b0d228c927bc8170a95974eaa8aeb98e6ee34535
Minseok Choe <choems@outlook.com>
2024-07-30 13:42:00 +0900

    Edit allowedOrigins

--- src/main/java/com/example/Devkor_project/configuration/WebConfig.java
+++ src/main/java/com/example/Devkor_project/configuration/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 /*
     CORS 설정
 */
@@ -20,15 +19,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry  registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
                 .allowedHeaders("*")
                 .allowCredentials(true)
-                .allowedOrigins("https://localhost:4000", "http://localhost:4000","https://localhost:3000", "http://localhost:3000", "https://127.0.0.1:4000",
-                        "http://127.0.0.1:4000","https://127.0.0.1:3000",
-                        "http://127.0.0.1:3000", "https://academ-frontend.vercel.app", "https://13.124.249.107.nip.io") // added more origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Wildcard method not works in some browsers, and HTTPS
+                .allowedOrigins("https://localhost:4000", "http://localhost:4000", "https://localhost:3000",
+                        "http://localhost:3000", "http://localhost", "https://localhost",
+                        "https://academ-frontend.vercel.app", "https://13.124.249.107.nip.io") // added more origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Wildcard method not works in some browsers, and
+                                                                 // HTTPS
     }
 
 }
